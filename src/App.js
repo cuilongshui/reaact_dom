300dad779836e55306cee0563640497ba77077bb
cuilongshui <515158150@qq.com>
2021-01-01 11:15:23 +0800

    state

--- src/App.js
+++ src/App.js
@@ -2,41 +2,53 @@
  * @Author: cui
  * @Date: 2020-12-04 23:02:56
  * @LastEditors: cui
- * @LastEditTime: 2020-12-13 10:05:03
+ * @LastEditTime: 2020-12-13 12:17:44
  * @Description: 
  */
 import "./App.css";
 import ListItem from "./components/listItem";
 import React, { Component } from 'react';
 
-
-const listData = [
-  {
-    id: 1,
-    title: "sony 65寸高清电视666666",
-    price: 4000,
-  },
-  {
-    id: 2,
-    title: "华为 Meta30",
-    price: 6000,
-  },
-  {
-    id: 3,
-    title: "华硕 玩家国度笔记本",
-    price: 10000,
-  },
-];
-
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+
+    this.state = {
+      listData: [
+        {
+          id: 1,
+          title: "sony 65寸高清电视666666",
+          price: 4000,
+        },
+        {
+          id: 2,
+          title: "华为 Meta30",
+          price: 6000,
+        },
+        {
+          id: 3,
+          title: "华硕 玩家国度笔记本",
+          price: 10000,
+        },
+      ]
+    }
+  }
+
+
   randerList() {
-    return listData.map(item => {
+    return this.state.listData.map(item => {
       return <ListItem key={item.id} data={item} onDelete={this.handleDelete} />
     })
   }
 
   handleDelete = (id) => {
-    console.log('id:',id)
+    const listData = this.state.listData.filter(item => item.id !== id);
+    this.setState((state,props)=>{
+        return{
+            listData
+        }
+    })
   }
 
   render() {
@@ -44,7 +56,7 @@ class App extends Component {
     return (
       <div className="container">
         <span className='title'>header</span>
-        {!listData.length && <div className='text-center'>购物车是空的</div>}
+        {!this.state.listData.length && <div className='text-center'>购物车是空的</div>}
         {this.randerList()}
       </div>
     );
--- src/components/listItem.jsx
+++ src/components/listItem.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import style from "./listItem.module.css";
 import classnames from "classnames/bind";
 import cn from "classnames";
 
 const cls = classnames.bind(style);
-class ListItem extends Component {
+class ListItem extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
@@ -21,11 +21,12 @@ class ListItem extends Component {
   }
 
   handelDecrease = (id) => {
-    this.setState({
-      count: this.state.count - 1,
-    }, () => {
-        
-    });
+    this.setState(
+      {
+        count: this.state.count - 1,
+      },
+      () => {}
+    );
   };
 
   handelIncrease = () => {
@@ -33,10 +34,22 @@ class ListItem extends Component {
       count: this.state.count + 1,
     });
   };
+  //重新渲染reader方法执行前  避免不必要的渲染
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log("props", this.props, nextProps);
+  //   console.log("state", this.state, nextState);
+
+  //   if (this.state.count === nextState.count) return false;
+  //   return true;
+  // }
+
+
+
 
   render() {
+    console.log("listItem rendering");
     const _cn = cn({
-      "themed-grid-col-s": !count,
+      "themed-grid-col-s": !this.state.count,
     });
 
     return (
@@ -66,6 +79,9 @@ class ListItem extends Component {
           >
             +
           </button>
+        </div>
+        <div className="col-2 themed-grid-col ">
+          ￥ {this.props.data.price * this.state.count}
         </div>
         <div className="col-1 themed-grid-col">
           <button
